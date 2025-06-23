838c822f5147a7569cf52fc66a5915d9f74de234
SRIKOTI-SIRI-KRISHNA <srikotisirikrishna@gmail.com>
2025-06-23 23:03:50 +0530

    intial commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express=require("express")
 // import express from "express";
 const app = express();
 app.get('/api/notes',(req,res)=>{
-    res.send('This is a get method api/notes')
+    res.send('This is a get method api/notes to view your notes')
 })
 app.listen(5000, () => {
   console.log("Listening to server ");
