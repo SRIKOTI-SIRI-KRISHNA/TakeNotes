17b41767f0e1fe62d1bdcf0e1ccb3fc48963ad52
SRIKOTI-SIRI-KRISHNA <srikotisirikrishna@gmail.com>
2025-06-24 09:03:04 +0530

    initial commit

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const app = express();
 app.get('/api/notes',(req,res)=>{
     res.send('This is a get method api/notes to view your notes')
 })
+app.post('/api/createnotes',(req,res)=>{
+    res.send('This is a post method api/createnotes to create your notes')
+})
+app.put('/api/updatenotes',(req,res)=>{
+    res.send('This is a put method api/updatenotes to update your notes')
+})
+app.delete('/api/delnotes',(req,res)=>{
+    res.send('This is a delete method api/notes to delete your notes')
+})
 app.listen(5000, () => {
   console.log("Listening to server ");
 });
